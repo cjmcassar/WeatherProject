1dea4473cfc0d14228bb047b34ebce13c1b1c48e
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-10-31 19:25:30 -0400

    - implemented body parser to parse post requests to the server

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res)
 {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b261c422b5469b94d4dbec738d8615de&units=metric"
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res)
+{
+  const query = req.body.cityName;
+  const apikey = "b261c422b5469b94d4dbec738d8615de"
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
   https.get(url, function(response)
   {
     console.log(response.statusCode);
@@ -18,13 +29,12 @@ app.get("/", function(req, res)
       const icon = weatherData.weather[0].icon
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
       res.write("<p>The weather is currently " + weatherDescription + "<p>");
-      res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
       res.write("<img src=" + imageURL + ">");
       res.send()
     })
   })
-});
-
+})
 
 app.listen(3000, function()
 {
