30961294ffec760fee05391b8991917eefc2a46d
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-10-30 20:36:35 -0400

    - added temperature information and weather description from api to response function

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ app.get("/", function(req, res)
   const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b261c422b5469b94d4dbec738d8615de&units=metric"
   https.get(url, function(response)
   {
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function(data)
+    {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      console.log(temp);
+      console.log(weatherDescription);
+    })
   })
   res.send("Server is up and running")
 });
